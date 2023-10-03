f1f178c7d4c221ace6480cbf09bddf5dbbdb8be4
jesusv1597 <146861675+jesusv1597@users.noreply.github.com>
2023-10-03 12:04:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jesus Velez"; // update the part BETWEEN the "quotes"
+let userName = "jesusv1597"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
